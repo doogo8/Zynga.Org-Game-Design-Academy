3e57bbb5a98a534fc80c00380153905ae2c5b834
Jesse Coats <jcoats@zynga.com>
2016-05-02 14:57:50 -0700

    Added scene: 2D Hit Detection.

---
+++ Tutorials/Assets/Scripts/Collision/HitDetection.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+import UnityEngine.Physics2D;
+
+/// <summary>
+/// Detects mouse clicks on a collider in a 2D scene 
+/// Outputs the name of the collider to the console
+/// Author: wilcoats
+/// </summary>
+public class HitDetection extends MonoBehaviour {
+
+	function Update () {
+		if(Input.GetMouseButtonDown(0))
+		{
+			// Cast a ray towards infinity from the mouse click location
+		    var ray: Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		    // Detect if the ray intersected a collider
+	    	var hit: RaycastHit2D = Physics2D.GetRayIntersection(ray,Mathf.Infinity);
+
+			// Check if the collider is this one		           
+		    if(hit.collider != null && hit.collider.transform == this.transform)
+		    {
+				Debug.LogFormat("Hit Detected on {0}.", hit.collider.transform.gameObject.name);
+			}
+		}	
+	}
+}
